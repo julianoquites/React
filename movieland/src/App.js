9d06bc21f37b03624e99bbd222fd9dbdf4ea7bec
Juliano Quites <juliano.quites@gmail.com>
2023-03-29 00:40:34 -0300

    changed the no movies found message
    changed the no movies found message to only show after searching for something

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -1,32 +1,41 @@
-import "./App.css";
-import { useState } from "react";
-import MovieCard from "./MovieCard";
-import SearchIcon from "./search.svg";
+import './App.css';
+import { useState } from 'react';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg';
 
-const API_URL = "http://www.omdbapi.com?apikey=cab58688";
-let wasPressed = false;
+const API_URL = 'http://www.omdbapi.com?apikey=cab58688';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('')
-
-  const handlePress = (e) => {
-    if (e.key === "Enter") {
-      searchMovies(searchTerm);
-      wasPressed = true
-    }
-  };
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searched, setSearched] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const searchMovies = async (title) => {
+    setLoading(true);
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
+    setLoading(false);
+  };
+
+  const handlePress = (e) => {
+    if (e.key === 'Enter') {
+      setSearched(true);
+      searchMovies(searchTerm);
+      setSearchTerm('');
+    }
+  };
+
+  const handleSearchClick = () => {
+    searchMovies(searchTerm);
+    setSearched(true);
+    setSearchTerm('');
   };
 
   return (
     <div className='app'>
       <h1>MovieLand</h1>
-
       <div className='search'>
         <input
           placeholder='Search for movies'
@@ -34,26 +43,23 @@ function App() {
           onKeyDown={handlePress}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img
-          src={SearchIcon}
-          alt='search'
-          onClick={() => searchMovies(searchTerm)}
-        />
+        <img src={SearchIcon} alt='search' onClick={handleSearchClick} />
       </div>
-      {wasPressed !== true ? 
+      {loading ? (
         <div className='empty'>
-        </div> :
-        movies?.length > 0 ? (
+          <h2>Loading...</h2>
+        </div>
+      ) : movies?.length > 0 ? (
         <div className='container'>
           {movies.map((movie) => (
             <MovieCard movie={movie} key={movie.imdbID} />
           ))}
         </div>
-      ) : (
+      ) : searched ? (
         <div className='empty'>
           <h2>No movies found</h2>
         </div>
-      )}
+      ) : null}
     </div>
   );
 }
